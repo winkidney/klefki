70ccbb0a7ae9cd8ba897d696a1b11cec7075bdd8
winkidney <winkidney@gmail.com>
2019-11-15 18:42:21 +0800

    Feature: Try to add VSS

--- klefki/crypto/ssss.py
+++ klefki/crypto/ssss.py
@@ -13,6 +13,11 @@ from klefki.types.algebra.fields import FiniteField
 from klefki.types.algebra.meta import field
 from klefki.types.algebra.utils import randfield
 
+
+def get_v_i(g, a_i):
+    return g * a_i
+
+
 class SSSS:
     def __init__(self, F: FiniteField):
         self.F = F
@@ -21,10 +26,26 @@ class SSSS:
     def setup(self, Secret, k, n):
         self.k = k
         self.n = n
+        self.g = randfield(self.F)
         a = [randfield(self.F) for _ in range(k - 1)]
+        self.v_list = [get_v_i(self.g, a_i) for a_i in a]
+        self._powers = [(i, i) for i in range(1, k - 1)]
         self.f = lambda x: Secret + reduce(
-            add, [a[i] * (x ** i) for i in range(1, k - 1 )])
+            add, [a[index] * (x ** power) for index, power in self._powers])
         return self
+
+    def get_v_i_to_verify(self, i):
+        return self.v_list[i]
+
+    def verify(self, v_to_verify, i):
+        v_j = None
+        for index, j in self._powers:
+            if v_j is None:
+                v_j = self.v_list[j] ** (i ** j)
+            else:
+                v_j *= self.v_list[j] ** (i ** j)
+        product = v_j
+        return product
 
     def join(self):
         assert self.node_count < self.n
--- tests/crypto/test_ssss.py
+++ tests/crypto/test_ssss.py
@@ -16,3 +16,19 @@ def test_ssss():
     assert s.decrypt([s.join() for _ in range(k - 1)]) != secret
     assert s.decrypt([s.join() for _ in range(k)]) == secret
     assert s.decrypt([s.join() for _ in range(k + 1)]) == secret
+
+
+def test_vss():
+    F = field(P)
+    s = SSSS(F)
+    k = random.randint(1, 100)
+    n = k * 3
+    secret = randfield(F)
+
+    s.setup(secret, k, n)
+
+    assert s.decrypt([s.join() for _ in range(k - 1)]) != secret
+    assert s.decrypt([s.join() for _ in range(k)]) == secret
+    assert s.decrypt([s.join() for _ in range(k + 1)]) == secret
+    v_to_verify = s.get_v_i_to_verify(2)
+    assert s.verify(v_to_verify, 2) == v_to_verify
